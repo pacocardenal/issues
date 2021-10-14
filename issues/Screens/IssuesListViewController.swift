17a876ce7b7a1835855dac0fa12710e16a8b5990
Paco Cardenal <franciscojavier.cardenal@cognizant.com>
2021-10-14 13:34:58 +0200

    Search functionality

--- issues/Screens/IssuesListViewController.swift
+++ issues/Screens/IssuesListViewController.swift
@@ -15,6 +15,7 @@ final class IssuesListViewController: UIViewController {
   
   var collectionView: UICollectionView!
   var issues = [Issue]()
+  var filteredIssues = [Issue]()
   var dataSource: UICollectionViewDiffableDataSource<Section, Issue>!
   
   override func viewDidLoad() {
@@ -22,6 +23,7 @@ final class IssuesListViewController: UIViewController {
     
     configureViewController()
     configureCollectionView()
+    configureSearchController()
     configureDataSource()
     getIssues()
     
@@ -39,6 +41,15 @@ final class IssuesListViewController: UIViewController {
     collectionView.register(IssueCell.self, forCellWithReuseIdentifier: IssueCell.reuseId)
   }
   
+  private func configureSearchController() {
+       let searchController = UISearchController()
+       searchController.searchBar.delegate = self
+       searchController.searchResultsUpdater = self
+       searchController.searchBar.placeholder = "Search..."
+       searchController.obscuresBackgroundDuringPresentation = false
+       navigationItem.searchController = searchController
+   }
+  
   private func configureDataSource() {
     dataSource = UICollectionViewDiffableDataSource(collectionView: collectionView,
                                                     cellProvider: { (collectionView, indexPath, issue) -> UICollectionViewCell? in
@@ -55,7 +66,7 @@ final class IssuesListViewController: UIViewController {
     })
   }
   
-  private func updateData() {
+  private func updateData(on issues: [Issue]) {
     var snapshot = NSDiffableDataSourceSnapshot<Section, Issue>()
     snapshot.appendSections([.main])
     snapshot.appendItems(issues)
@@ -80,12 +91,33 @@ final class IssuesListViewController: UIViewController {
           }
           return
         }
-        self.updateData()
+        self.updateData(on: issues)
       case .failure(let error):
         self.presentISAlertOnMainThread(title: "Bad stuff happened", message: error.rawValue, buttonTitle: "Ok")
       }
     }
   }
+  
+}
+
+extension IssuesListViewController: UISearchBarDelegate, UISearchResultsUpdating {
+  
+  func updateSearchResults(for searchController: UISearchController) {
+       guard let filter = searchController.searchBar.text, !filter.isEmpty else { return }
+
+       filteredIssues = issues.filter { $0.firstName?.lowercased().contains(filter.lowercased()) ?? false }
+       updateData(on: filteredIssues)
+   }
+
+   func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+       updateData(on: issues)
+   }
+  
+  func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+       if searchText.isEmpty {
+           updateData(on: issues)
+       }
+   }
   
 }
 
